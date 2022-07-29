83a7cec8ed0f248efaafc62c668306c584a8b024
hmtmcse <hmtm.cse@gmail.com>
2022-07-30 00:51:07 +0600

    added filter

--- pf_pweb_notify/pfpn_pweb_notify.py
+++ pf_pweb_notify/pfpn_pweb_notify.py
@@ -18,7 +18,7 @@ class PWebNotify:
     @staticmethod
     def send_data(event, data, namespace: str = None, broadcast: bool = False):
         params = {}
-        return
+        # return
         if namespace:
             params["namespace"] = namespace
         if broadcast:
